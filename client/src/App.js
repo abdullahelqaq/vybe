f1d4df0b15b581887320241d3468ac952ca04cea
Anchal Sinha <43023676+anchalsinha@users.noreply.github.com>
2021-04-28 23:40:59 -0700

    Spotify authorization completed

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
 
+export const authEndpoint = 'https://accounts.spotify.com/authorize';
+
+const clientId = "62598dfbbefd46eeb90783eb0b6d0ad9";
+const redirectUri = "http://localhost:3000/authorized/";
+//TODO: INSERT SCOPES: "https://developer.spotify.com/documentation/general/guides/scopes/
+const scopes = [];
+
+const url = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=code&show_dialog=true`;
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          vybe
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className="btn btn--loginApp-link"
+          href={url}
         >
-          Learn React
+          Login to Spotify
         </a>
       </header>
     </div>
---
+++ session.js
@@ -0,0 +1,2 @@
+const Spotify = require('spotify-web-api-node');
+
