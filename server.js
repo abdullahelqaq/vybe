10fb8dff5b0f6fe7e8da33f79c2cb9821728ac24
Abdullah Elqaq <abdullahelqaq@gmail.com>
2021-04-28 18:37:09 -0700

    [init] Initialized backend and frontend in subdirectories (#1)

---
+++ server.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const express = require("express");
+const api = require('./routes/routes');
+const cors = require('cors');
+
+const port = process.env.PORT || 3000;
+
+const app = express();
+app.use(cors());
+
+app.use(express.static(path.resolve(__dirname, './client/build')));
+app.use('/', api);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Server running on ${port}`);
+});
