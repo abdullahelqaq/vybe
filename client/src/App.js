04affd8750cf93c65c645ae324fff149072849f4
Abdullah Elqaq <abdullah@radiirobotics.com>
2021-05-16 23:21:08 -0700

    Added login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,30 @@
 import React from 'react';
 import './App.css';
 import * as spotify from './spotify.js';
+import Slider from '@material-ui/core/Slider';
+
+function renderQueueEntries(songs, count=-1) {
+  var rows = []
+  var limit = songs.length;
+
+  if (count !== -1 && count < songs.length) {
+    limit = count;
+  }
+
+  for (let i = 0; i < limit; i++) {
+    rows.push(
+      <p key={i}><b>{songs[i].name}</b> / {songs[i].artist}</p>
+    );
+  }
+  return rows;
+}
 
 class Queue extends React.Component {
   constructor(props) {
     super(props);
-
     this.state = {
-      songs: [{name: "Heat Waves", artist: "Glass Animals"}],
       input_active: false,
     };
-
   }
 
   stopExitClick(e) {
@@ -21,9 +35,7 @@ class Queue extends React.Component {
   }
 
   checkExitClick() {
-    console.log("here");
     if (!this.state.input_active) {
-      console.log("leaving");
       this.props.onClick();
     } else {
       this.setState({
@@ -33,7 +45,7 @@ class Queue extends React.Component {
   }
 
   render() {
-    if (this.state.songs.length === 0) {
+    if (this.props.queue.length === 0) {
       return (
         <div className="Queue" onClick={this.checkExitClick.bind(this)}>
           <div className="Queue-Empty">
@@ -59,11 +71,50 @@ class Queue extends React.Component {
             />
             <Player current_song={this.props.current_song} />
           </div>
+          <div className="Queue-Body">
+            <h2 className="Header-Left">Queue</h2>
+            {renderQueueEntries(this.props.queue)}
+          </div>
         </div>
       );
     }
   }
+}
 
+class MoodControl extends React.Component {
+
+  renderParamSliders(params) {
+    var sliders = []
+    console.log(params.length);
+    for (let i = 0; i < params.length; i++) {
+      sliders.push(
+        <div key={i}>
+          <h4>{params[i].name}</h4>
+          <p />
+          <Slider
+            disabled
+            orientation="vertical"
+            style={{height: "50px", color: "white", display: "table", margin: "0 auto"}}
+            max={1}
+            value={params[i].value}
+          />
+        </div>
+      );
+    }
+    return sliders;
+  }
+
+  render () {
+    return (
+      <div className="Queue" onClick={() => this.props.onClick()}>
+        <h2 className="Header-Left">{this.props.mood}</h2>
+        <div className="Param-Grid">
+          {this.renderParamSliders(this.props.params)}
+        </div>
+      </div>
+    );
+    
+  }
 }
 
 function Player(props) {
@@ -85,7 +136,22 @@ class App extends React.Component {
       current_song: {
         name: "Heat Waves",
         artist: "Glass Animals"
-      }
+      },
+      queue: [
+        {name: "Bad Decisions", artist: "The Strokes"},
+        {name: "Shy Away", artist: "Twenty One Pilots"},
+      ],
+      mood: "Dancey",
+      mood_params: [
+        {name: "Accousticness", value: 0.9},
+        {name: "Danceability", value: 0.2},
+        {name: "Instrumental", value: 0.3},
+        {name: "Energy", value: 0.8},
+        {name: "Loudness", value: 0.7},
+        {name: "Speechiness", value: 0.2},
+        {name: "Tempo", value: 0.9},
+        {name: "Valence", value: 0.4},
+      ],
     }
   }
 
@@ -116,38 +182,102 @@ class App extends React.Component {
     this.setActivePage(0);
   }
 
+  renderQueueWidget() {
+    if (this.state.queue.length > 0) {
+      return (
+      <div className="App-widget" onClick={() => this.setActivePage(1)}>
+        <h2>Queue</h2>
+        {renderQueueEntries(this.state.queue, 5)}
+      </div>
+      );
+    } else {
+      return (
+      <div className="App-widget" onClick={() => this.setActivePage(1)}>
+        <h2>Queue</h2>
+        <h3>Tap to Add Songs</h3>
+      </div>
+      );
+    }
+  }
+
+  renderMoodWidget() {
+    if (!this.state.mood) {
+      return (
+        <div className="App-widget">
+          <h2>Mood</h2>
+          <h4>Queue more songs for vybe to learn your mood!</h4>
+        </div>
+      );
+    }
+    else {
+      return (
+        <div className="App-widget" onClick={() => this.setActivePage(2)}>
+          <h2>Mood</h2>
+          <h4>vybe feels that you're</h4>
+          <h3>{this.state.mood.toUpperCase()}</h3>
+        </div>
+      );
+    }
+  }
+
   render() {
 
-    if (this.state.active_page === 0) {
+    let endpoint = "http://www.google.com";
+
+    if (!this.state.token) {
       return (
         <div className="App">
-          {this.renderDashboardHeader()}
-          <div className="App-widget" onClick={() => this.setActivePage(1)}>
-            <h2>Queue</h2>
-            <h4>UP NEXT</h4>
-            <h3>Bad Decisions / The Strokes</h3>
-          </div>
-          <div className="App-widget">
-            <h2>Mood</h2>
-            <h4>vybe feels that you're</h4>
-            <h3>DANCEY</h3>
+          <div className="App-Login">
+            <div className="Login-Wrapper">
+              <h1>vybe</h1>
+              <h2>Tunes Tailored to You</h2>
+              <br /><br />
+              <form method="GET" action={endpoint} className="Invisible-Form">
+                <input type="submit" className="App-Login-Button" value="LOGIN" />
+              </form>
+              <br /><br /><br /><br /><br /><br />
+            </div>
           </div>
         </div>
       );
-    } else if (this.state.active_page === 1) {
-      return (
-      <div className="App">
-        <Queue
-          onClick={this.returnToDashboard.bind(this)}
-          current_song={this.state.current_song}
-        />
-      </div>
-      );
-
     } else {
-      return (
-        <div>Current Active Page Index: {this.state.active_page}</div>
-      );
+      // logged in
+      if (this.state.active_page === 0) {
+        return (
+          <div className="App">
+            {this.renderDashboardHeader()}
+            <br />
+            {this.renderQueueWidget()}
+            {this.renderMoodWidget()}
+          </div>
+        );
+      } else if (this.state.active_page === 1) {
+        return (
+        <div className="App">
+          <Queue
+            onClick={this.returnToDashboard.bind(this)}
+            current_song={this.state.current_song}
+            queue={this.state.queue}
+          />
+        </div>
+        );
+  
+      } else if (this.state.active_page === 2) {
+        return (
+        <div className="App">
+          <MoodControl
+            onClick={this.returnToDashboard.bind(this)}
+            mood={this.state.mood}
+            params={this.state.mood_params}
+          />
+        </div>
+        );
+      }
+      else {
+        return (
+          <div>Current Active Page Index: {this.state.active_page}</div>
+        );
+      }
     }
 
 
