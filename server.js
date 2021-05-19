ff7f78314ed9238e51b055ed7705b88ed6ea9b61
Anchal Sinha <43023676+anchalsinha@users.noreply.github.com>
2021-05-18 23:27:38 -0700

    Removed need for duplicate values for 'track_id' and 'id'

--- server.js
+++ server.js
@@ -59,7 +59,8 @@ router.get('/dashboard', async (req, res) => {
 // Request body must contain the session id and list of song IDs
 router.post('/setSeedSongs', (req, res) => {
   const id = req.query.id;
-  userSessions[id].setSeedSongs(req.body.songs);
+  if (id in userSessions)
+    userSessions[id].setSeedSongs(req.body.songs);
 });
 
 // POST /skip
@@ -69,14 +70,17 @@ router.post('/skip', (req, res) => {
   const id = req.query.id;
   const songId = req.body.id;
   const feedback = req.body.feedback;
-  userSessions[id].skipSong(songId, feedback);
+  if (id in userSessions)
+    userSessions[id].skipSong(songId, feedback);
 });
 
 // POST /finish
 // Endpoint called when the song finishes playing
 router.post('/finish', (req, res) => {
+  const id = req.query.id;
   const songId = req.query.id;
-  userSessions[id].finishSong(songId);
+  if (id in userSessions)
+    userSessions[id].finishSong(songId);
 });
 
 
@@ -86,7 +90,8 @@ router.post('/finish', (req, res) => {
 // Response body contains status code 0 (working) or 1 (completed)
 router.get('/status', (req, res) => {
   const id = req.query.id;
-  res.body = JSON.stringify({ status: userSessions[id].status });
+  if (id in userSessions)
+    res.body = JSON.stringify({ status: userSessions[id].status });
 });
 
 // GET /queue
@@ -95,8 +100,10 @@ router.get('/status', (req, res) => {
 // Response body contains list of song IDs
 router.get('/queue', (req, res) => {
   const id = req.query.id;
-  res.body = JSON.stringify({ queue: userSessions[id].queue });
-  userSessions[id].status = 0;
+  if (id in userSessions) {
+    res.body = JSON.stringify({ queue: userSessions[id].queue });
+    userSessions[id].status = 0;
+  }
 });
 
 // GET /preferences
@@ -106,7 +113,8 @@ router.get('/queue', (req, res) => {
 router.get('/preferences', (req, res) => {
   const id = req.query.id;
   res.body = JSON.stringify({ preferences: userSessions[id].preferences });
-  userSessions[id].status = 0;
+  if (id in userSessions)
+    userSessions[id].status = 0;
 });
 
 
